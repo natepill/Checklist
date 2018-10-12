962df012b2dc160a29e2c6a6971cf68d452ad902
Nathan Pillai <natepill@gmail.com>
2018-10-12 12:26:30 -0700

    bug fix

--- checklist.py
+++ checklist.py
@@ -1,77 +1,72 @@
+
 checklist = list()
 
 def create(item):
-  checklist.append(item)
+    checklist.append(item)
+    list_all_items()
 
 def read(index):
-  return checklist[index]
+    return checklist[int(index)]
 
 def update(index, item):
-  checklist[index] = item
+    checklist[index] = item
 
 def destroy(index):
-  checklist.pop(index)
+    checklist.pop(index)
 
 def list_all_items():
-  index = 0
-  for list_item in checklist:
-    print("{} {}".format(index, list_item))
-    index += 1
+    index = 0
+    for list_item in checklist:
+        print("{} {}".format(index, list_item))
+        index += 1
 
 def mark_completed(index):
-  print("* {}".format(checklist[index]))
-
-running = True
+    checklist[index] = "DONE: " + checklist[index]
 
 def select(function_code):
-    if function_code == ("C" or "c"):
-        print('Function code: C')
-        input_item = user_input("Input Item: ")
+    # Create item
+    if function_code == "C" or "c":
+        input_item = user_input("Input item: ")
         create(input_item)
 
-    elif function_code == ("R" or "r"):
-        print('Function Code R')
-        item_index = int(user_input("Index Number? "))
-        if item_index > (len(checklist))-1:
-            print("Invalid Index")
-            return True
-        read(item_index)
+    # Read item
+    elif function_code == "R" or "r":
+            item_index = user_input("Index Number? ")
+            # Remember that item_index must actually exist or our program will crash.
+            print(read(item_index))
 
-    elif function_code == ("P" or "p"):
-        print('Function Code P')
-        list_all_items()
+        # Print all items
+    elif function_code == "P" or "p":
+            list_all_items()
 
-    elif function_code == ("Q" or "q"):
-        print(running)
-        return False
+        # Stop our loop
+    elif function_code == "Q" or "q":
+            return False
 
+        # Update
+    elif function_code == "U":
+        item_index = user_input("Index Number: ")
+        input_item = user_input("Input item: ")
+        update(item_index, input_item)
+
+    # Catch all
     else:
-        print("unknown option!")
+        print("Unknown Option")
+
     return True
 
-
-
 def user_input(prompt):
-    user_input = raw_input(prompt)
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
     return user_input
 
-
-
+running = True
+# print("running is {}".format(running))
 while running:
-    selection = user_input("Press C to add to list, R to read from list, P to display list ")
-    print(selection)
-
-    if selection == ("Q" or "q"):
-        break
-
-    runnning = select(selection)
-    print(running)
-
-
-    # print(running)
-
-
-
+    selection = user_input(
+        "Press C to add to list, U to update, R to Read items, P to display, Q to quit")
+    running = select(selection)
 
 
 
